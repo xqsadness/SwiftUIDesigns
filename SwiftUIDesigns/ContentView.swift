873fa93fc64875d7e0558852f772826d46d2b0f5
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-05-27 08:57:26 +0700

    first commit

---
+++ SwiftUIDesigns/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIDesigns
+//
+//  Created by darktech4 on 27/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIDesigns/SwiftUIDesignsApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIDesignsApp.swift
+//  SwiftUIDesigns
+//
+//  Created by darktech4 on 27/05/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftUIDesignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
