251ff17843cffce3bb6f648b9ca835bcb6de0e9a
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-05-27 17:15:46 +0700

    add from photo

---
+++ SwiftUIDesigns/Core/ImageGalleryApp/Helpers/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  SwiftUIDesigns
+//
+//  Created by darktech4 on 27/05/2024.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
